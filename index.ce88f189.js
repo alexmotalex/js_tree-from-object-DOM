b0d87df705efb5391509f8725afb303f23ee824e
Alex <tokyopac@gmail.com>
2024-09-06 22:47:30 +0300

    make build

---
+++ index.ce88f189.js
@@ -0,0 +1,2 @@
+var r;r={Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}},document.querySelector("#tree").innerHTML=function r(e){var n,a="";for(var i in e)(a+="<li>"+i+r(e[i])+"</li>")&&(n="<ul>"+a+"</ul>");return n||""}(r);
+//# sourceMappingURL=index.ce88f189.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,33 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    element.innerHTML = createTreeDom(data);
+}
+createTree(tree, food);
+function createTreeDom(obj) {
+    let li = "";
+    let ul;
+    for(const key in obj){
+        li += "<li>" + key + createTreeDom(obj[key]) + "</li>";
+        if (li) ul = "<ul>" + li + "</ul>";
+    }
+    return ul || "";
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
